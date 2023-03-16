6235a03e61f79253ddabde8af604af7dd44b7033
FrJallah <frj.mitnick@gmail.com>
2023-03-16 16:52:48 -0700

    Hello World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line with puts functin
+ * main - A program that prints a line with puts functi
+n
  * Return: 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzz
+le");
 return (0);
 }

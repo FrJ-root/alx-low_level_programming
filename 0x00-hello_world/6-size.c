fd1b26f6c04f80163fe89f826bd95f8e00fb0400
FrJallah <frj.mitnick@gmail.com>
2023-03-19 05:14:11 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,4 +17,5 @@ printf("Size of a long int: %lu byte(s)", (unsigned long)sizeof(c));
 printf("Size of a long long int: %lu byte(s)", (unsigned long)sizeof(d));
 printf("Size of a float: %lu byte(s)", (unsigned long)sizeof(f));
 return (0);
-}
+} 
+

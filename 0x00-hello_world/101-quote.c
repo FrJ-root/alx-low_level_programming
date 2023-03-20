16fe85f7fb9023373a01cd86f97e59c1fab9e2a1
FrJallah <frj.mitnick@gmail.com>
2023-03-20 06:14:53 -0700

    Hello World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,11 @@
-#include<stdio.h>
-#include<unistd.h>
-
+#include <stdio.h>
+#include <unistd.h>
 /**
- * main - Entry point
- *
- * Return: Always 1 (Success)
+ * main - A c program that prints a line to the standard error
+ * Return 1 (Success)
  */
 int main(void)
 {
-	write(2,
-	"and that piece of art is useful\" - Dora kopar, 2015-10-19\n", 59);
-	return (1);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }

a9580c6a11db474b3b0f1648e417ef81501e704d
FrJallah <frj.mitnick@gmail.com>
2023-03-22 16:11:38 -0700

    functions nested

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,3 @@
-#ifndef main_h
-#define main_h
-
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -15,4 +12,3 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#endif

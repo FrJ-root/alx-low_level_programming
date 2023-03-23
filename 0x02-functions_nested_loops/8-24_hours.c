9914b9c92a7c04bb94a09041e05837556e28bbcc
FrJallah <frj.mitnick@gmail.com>
2023-03-22 18:37:34 -0700

    Update

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,35 +1,25 @@
-#include "main.h"
-
-/** 
- * jack_bauer - Prints every minute of the day of Jack Bauer
- * Return: ...
+#include "holberton.h"
+/**
+ * jack_bauer - function that prints every minute of the day, 24 hour clock
+ * h = hour, m = minutes
+ * / 10 allows second digit to rotate
+ * for loop breaks before passing 24:00
+ * Return: 24 hour clock line by line
  */
-
 void jack_bauer(void)
-
 {
+	int h, m;
 
-        int a, b, c, d;
-
-        for (a = 0; a <= 2; a++)
-        {
-	for (b = 0; b <= 9; b++)
+	for (h = 0; h < 24; h++)
 	{
-	if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
-	{
-	for (c = 0; c <= 5; c++)
-	{
-	for (d = 0; d <= 9; d++)
-	{
-	_putchar(a + '0');
-	_putchar(b + '0');
-	_putchar(58);
-	_putchar(c + '0');
-	_putchar(d + '0');
-	_putchar('\n');
-	}
-	}
-	}
-	}
+		for (m = 0; m < 60; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
 	}
 }
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,40 +1,39 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * times_table - Prints the 9 times table, starting with 0
- * Return: empty output
+ * times_table -  a function that prints the 9 times table, starting with 0
+ * rone = row, cone = column, d = digits of current result
+ * Return: times table
+ * add extra space past single digit
  */
-
 void times_table(void)
-
 {
-	int x, y, z, u, d;
+	int rone, cone, d;
 
-	for (x = 0; x <= 9; x++)
+	for (rone = 0; rone <= 9; rone++)
 	{
-	for (y = 0; y <= 9; y++)
-	{
-	z = x * y;
-	if (z > 9)
-	{
-	u = z % 10;
-	d = (z - u) / 10;
-	_putchar(44);
-	_putchar(32);
-	_putchar(d + 'a');
-	_putchar(u + 'a');
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (cone = 1; cone <= 9; cone++)
+		{
+			d = (rone * cone);
+			if ((d / 10) > 0)
+			{
+				_putchar((d / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((d % 10) + '0');
+
+			if (cone < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
 	}
-	else
-	{
-	if (y !=0)
-	{
-	_putchar(44);
-	_putchar(32);
-	_putchar(32);
-	}
-	_putchar(z + '0');
-	}
-	}
-	_putchar('\n');
-	}
+
 }

62fc1e3bb6ebf742ece58d3213172cfadb6ea87a
FrJallah <frj.mitnick@gmail.com>
2023-03-24 17:13:35 -0700

    debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,7 +6,6 @@
 */
 
 int main(void)
-
 {
         int i;
 
@@ -14,11 +13,10 @@ int main(void)
 
         i = 0;
 
-        /*while (i < 10)*/
-	/*{*/
-	/*
-                putchar(i);*/
-        /*}*/
+        while (i < 10)
+        {
+                putchar(i);
+        }
 
         printf("Infinite loop avoided! \\o/\n");
 
